7f7a1ac838d0a17be6721f20d4b6b9ffea2fe1ed
sujatagoudar123 <131695773+sujatagoudar123@users.noreply.github.com>
2026-05-29 20:22:56 +0530

    Update companies data with new spokespeople
    Added key spokespeople and influencers to various franchises.

--- data/companies.ts
+++ data/companies.ts
@@ -41,16 +41,16 @@ export const COMPANIES: Company[] = [
       // Categories
       'pharmaceutical', 'biotech', 'Pharma', 'Vaccine', 'Drug price',
       // Shingles franchise
-      'Shingles', 'SHINGRIX', 'herpes zoster', 'Thrive@50+',
+      'Shingles', 'SHINGRIX', 'herpes zoster', 'Thrive@50+', Leonard Friedland’, ‘Julie Bowen’, ‘Ty Burrell’, ‘Modern Family’
       // Meningitis franchise
-      'Meningitis', 'Meningococcal', 'Bexsero', 'Menveo', 'Ask2BSure',
+      'Meningitis', 'Meningococcal', 'Bexsero', 'Menveo', 'Ask2BSure', ‘Leonard Friedland’, ‘Julie Bowen’, ‘Ty Burrell’, ‘Modern Family’
       'MenABCWY', 'MenACWY',
       // RSV
-      'RSV', 'respiratory syncytial virus', 'Arexvy',
+      'RSV', 'respiratory syncytial virus', 'Arexvy', ‘Clesrovimab’, ‘Nirsevimab’, ‘Beyfortus’
       // Flu
-      'Influenza', 'Flu', 'Fluarix', 'Flulaval', 'FluMist',
+      'Influenza', 'Flu', 'Fluarix', 'Flulaval', 'FluMist', ‘Clesrovimab’, ‘Nirsevimab’, ‘Beyfortus’, ‘Ask2BSure’, ‘Penbraya’
       // Other diseases
-      'Malaria', 'Abrysvo',
+      'Abrysvo',
       // Key spokespeople / influencers from Detailed Search Terms
       'Len Friedland', 'Jenn Sherman', 'Arti Thangudu',
       'Lisa Breckenridge', 'Daisy Kent', 'Greg Olsen',
